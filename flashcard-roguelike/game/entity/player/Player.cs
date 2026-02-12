02ef9b78bce7e6150bec45c88c44fde6bbe2a5a8
ntassoneuwm <ntassone@uwm.edu>
2026-02-12 14:22:57 -0600

    feat: player pill, camera movement (mouse)

--- flashcard-roguelike/game/entity/player/Player.cs
+++ flashcard-roguelike/game/entity/player/Player.cs
@@ -1,15 +1,36 @@
 using Godot;
 using System;
 
-public partial class Player : Node
+public partial class Player : Node3D
 {
-	// Called when the node enters the scene tree for the first time.
+	[Export]
+	// Speed of camera
+	public float MouseSensitivity { get; set; } = 0.002f;
+
+	[Export]
+	// High/Low angle player can look (currently almost straight up/down)
+	public float MaxPitchDegrees { get; set; } = 89f;
+
+	private Node3D _cameraPivot;
+	private float _pitch;
+
 	public override void _Ready()
 	{
+		_cameraPivot = GetNode<Node3D>("CameraPivot");
+		Input.MouseMode = Input.MouseModeEnum.Captured;
 	}
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
+	public override void _Input(InputEvent @event)
 	{
+		if (@event is InputEventMouseMotion motion)
+		{
+			// Horizontal: rotate the whole player (yaw)
+			RotateY(-motion.Relative.X * MouseSensitivity);
+
+			// Vertical: rotate only the camera (pitch)
+			_pitch -= motion.Relative.Y * MouseSensitivity;
+			_pitch = Mathf.Clamp(_pitch, -Mathf.DegToRad(MaxPitchDegrees), Mathf.DegToRad(MaxPitchDegrees));
+			_cameraPivot.Rotation = new Vector3(_pitch, 0, 0);
+		}
 	}
 }
