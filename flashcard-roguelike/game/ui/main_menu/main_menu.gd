83fe7ced646d3536d13970de89b6bf90e0b1d6ef
LTEmmer <143912794+LTEmmer@users.noreply.github.com>
2026-02-13 01:59:41 -0600

    fix: changed the scene main menu loads on start, fixed the UI scaling to take the whole screen, and commented out debug code

--- flashcard-roguelike/game/ui/main_menu/main_menu.gd
+++ flashcard-roguelike/game/ui/main_menu/main_menu.gd
@@ -4,8 +4,8 @@ func _ready() -> void:
 	var play_button := get_node_or_null("CenterContainer/VBoxContainer/PlayButton") as Button
 	var quit_button := get_node_or_null("CenterContainer/VBoxContainer/QuitButton") as Button
 
-	print("play_button =", play_button)
-	print("quit_button =", quit_button)
+	#print("play_button =", play_button)
+	#print("quit_button =", quit_button)
 
 	if play_button:
 		play_button.pressed.connect(_on_play_pressed)
@@ -18,8 +18,8 @@ func _ready() -> void:
 		push_error("QuitButton not found")
 
 func _on_play_pressed() -> void:
-	print("HELLOOO")
-	get_tree().change_scene_to_file("res://scenes/main.tscn")
+	#print("HELLOOO")
+	get_tree().change_scene_to_file("res://game/entity/dungeon_generator/dungeon_generator.tscn")
 
 func _on_quit_pressed() -> void:
 	get_tree().quit()
