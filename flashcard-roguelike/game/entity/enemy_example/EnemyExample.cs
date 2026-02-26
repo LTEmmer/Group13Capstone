854bb591e26b4d4d6045e2e08e662f349f674bac
LTEmmer <143912794+LTEmmer@users.noreply.github.com>
2026-02-26 10:21:37 -0600

    fix: adjustments to positioning and rotation of battle area, restructured files, some more documentation

--- flashcard-roguelike/game/entity/enemy_example/EnemyExample.cs
+++ flashcard-roguelike/game/entity/enemy_example/EnemyExample.cs
@@ -29,7 +29,27 @@ public partial class EnemyExample : CharacterBody3D
 
 			// For now grab this specific enemy, but eventually we will want to grab all enemies in the room
 			// Maybe each room can store enemies in an enemies node so we can easily grab all enemies
-			// by: GetParent().GetNode("Enemies").GetChildren() or something like that and put them in a list.
+			// by: GetParent().GetChildren() or something like that and put them in a list. (since they are children of enemies)
+			// For example:
+
+			/*
+			List<EnemyExample> enemiesInRoom = new List<EnemyExample>();
+			foreach (var child in GetParent().GetChildren())
+			{
+				if (child is EnemyExample enemy)
+				{
+					enemiesInRoom.Add(enemy);
+				}
+				else
+				{
+					GD.PrintErr($"Non-enemy child detected in enemy container: {child.Name}");
+				}
+			}
+
+			You would get the room via GetParent().GetParent() assuming the structure is Room -> Enemies -> EnemyExample, 
+			and then get all enemies from the Enemies node. I tested it this way and it worked
+			*/
+
 			BattleManager.Instance.StartBattle(player, new List<EnemyExample> { this }, GetParent<Node3D>());
 		}
 	}
