be97748dca97b53b058e76bc22b9d4106757c5d9
LTEmmer <143912794+LTEmmer@users.noreply.github.com>
2026-03-11 21:23:53 -0500

    feat: basic flow of interaction and basicNPC

---
+++ flashcard-roguelike/ShootingEventRoom.cs
@@ -0,0 +1,53 @@
+using Godot;
+using System;
+
+public partial class ShootingEventRoom : Room, IEventRoom
+{
+    [Export] public BaseNPC TriggerNPC;
+    public bool IsCompleted { get; private set; }
+    public float Difficulty { get; private set; }
+
+    public override void _Ready()
+    {
+        base._Ready();
+        Difficulty = GameDifficultyManager.Instance.getCurrentDifficultyScore();
+
+        TriggerNPC.OnInteraction += TriggerEvent;
+    }
+
+    public void TriggerEvent()
+    {
+        // For testing, just complete the event immediately with success.
+        GD.Print("Shooting event triggered! Starting challenge...");
+        CompleteEvent(true);
+    }
+
+    public void CompleteEvent(bool success)
+    {
+        if (IsCompleted) return; // Prevent multiple completions
+        IsCompleted = true;
+
+        if (success)
+        {
+            ApplyReward();
+        }
+        else
+        {
+            ApplyPenalty();
+        }
+
+        EventManager.Instance.raise("on_room_clear", "test"); // Tell connections to open
+    }
+
+    public void ApplyReward()
+    {
+        GD.Print("Player has completed the shooting event room! Reward applied.");
+    }
+
+    public void ApplyPenalty()
+    {
+        GD.Print("Player has failed the shooting event room! Penalty applied.");
+    }
+
+
+}
---
+++ flashcard-roguelike/game/entity/npcs/BaseNPC.cs
@@ -0,0 +1,42 @@
+using System;
+using Godot;
+
+public partial class BaseNPC : Node3D
+{
+    [Signal]
+    public delegate void OnInteractionEventHandler();
+
+    private Area3D _area;
+    private bool _playerInRange = false;
+
+    public override void _Ready()
+    {
+        _area = GetNode<Area3D>("Area3D");
+        _area.BodyEntered += OnBodyEntered;
+        _area.BodyExited += OnBodyExited;
+    }
+
+    public override void _Input(InputEvent @event)
+	{
+		if (@event.IsActionPressed("interact") && _playerInRange)
+		{
+			EmitSignal(nameof(OnInteraction));
+		}
+	}
+
+    private void OnBodyEntered(Node body)
+    {
+        if (body is Node3D node && body.Name == "Player")
+        {
+            _playerInRange = true;
+        }
+    }
+
+    private void OnBodyExited(Node body)
+    {
+        if (body is Node3D node && node.Name == "Player")
+        {
+            _playerInRange = false;
+        }
+    }
+}
