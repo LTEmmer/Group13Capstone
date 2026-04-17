0e4ae1f644260acb4612372f151b695c5abb88aa
LTEmmer <143912794+LTEmmer@users.noreply.github.com>
2026-04-17 00:36:50 -0500

    fix: changed root of shake for boxes

--- flashcard-roguelike/game/entity/treasure/TreasureChest.cs
+++ flashcard-roguelike/game/entity/treasure/TreasureChest.cs
@@ -28,7 +28,7 @@ public partial class TreasureChest : Interactable
 
 	private Node3D _sacrificeNode;
 	private bool _resolved = false;
-    private MeshInstance3D _boxBody;
+    private Node3D _shakeNode;
 
 	public override void _Ready()
 	{
@@ -66,7 +66,7 @@ public partial class TreasureChest : Interactable
 		particleMat.Emission = RarityColors[_targetRarityIndex];
 		_particles.DrawPass1.SurfaceSetMaterial(0, particleMat);
 
-        _boxBody = GetNodeOrNull<MeshInstance3D>("Box");
+        _shakeNode = GetNodeOrNull<Node3D>("ShakeNode");
 	}
 
 	public override void Interact(Node caller) => OpenChest();
